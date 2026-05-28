ccd3d3d2d888001a95d27cb1f9875d37279e5ecd
Ruben Molina <rmolina@gmail.com>
2026-05-28 10:59:18 -0500

    Remove unused hydrology metrics helpers

--- src/devcon2026/hydrology/__init__.py
+++ src/devcon2026/hydrology/__init__.py
@@ -2,7 +2,6 @@
 
 from .export import HydrologyArtifactNames, export_nitrogen_hydrology_inputs
 from .io import load_forcing_data, load_observed_discharge, load_parameters
-from .metrics import align_series, nse, rmse
 from .model import Hydrology, synthetic_hydrology_forcing
 from .periods import PeriodRun, PeriodWindow, run_period
 from .simulation import simulate
@@ -18,15 +17,12 @@ __all__ = [
     "HydrologyParameters",
     "HydrologySimulationResult",
     "HydrologyStates",
-    "align_series",
     "export_nitrogen_hydrology_inputs",
     "load_forcing_data",
     "load_observed_discharge",
     "load_parameters",
-    "nse",
     "PeriodRun",
     "PeriodWindow",
-    "rmse",
     "run_period",
     "simulate",
     "synthetic_hydrology_forcing",
